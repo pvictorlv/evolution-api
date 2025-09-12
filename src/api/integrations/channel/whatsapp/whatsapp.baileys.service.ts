e77e40145a412ad1d3d19341bd329aed07a43235
Paulo Victor Lund <pvictorlv@gmail.com>
2025-09-12 13:46:40 -0300

    Update whatsapp.baileys.service.ts

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -3748,9 +3748,12 @@ export class BaileysStartupService extends ChannelStartupService {
             }
 
             if (typeof mediaMessage['mediaKey'] === 'object') {
+                console.log('mediaKey is an object, converting to string...');
+                console.log(msg.message);
                 msg.message = JSON.parse(JSON.stringify(msg.message));
             }
 
+            console.log('trying to download media message...');
             const buffer = await downloadMediaMessage(
                 {key: msg?.key, message: msg?.message},
                 'buffer',
