22c92c2afacb8cb43a96cf81439d401d3b4de3f2
Paulo Victor Lund <pvictorlv@gmail.com>
2025-09-25 21:38:57 -0300

    Update whatsapp.baileys.service.ts

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -1217,7 +1217,7 @@ export class BaileysStartupService extends ChannelStartupService {
             !received.message?.viewOnceMessage
           ) {
             console.log('protocolMessage or pollUpdateMessage or empty message, ignored', received);
-            console.log('protocolMessage or pollUpdateMessage or empty message, ignored', received.message?.peerDataOperationRequestResponseMessage);
+            console.log('peerDataOperationRequestResponseMessage', received.message?.peerDataOperationRequestResponseMessage);
             continue;
           }
 
