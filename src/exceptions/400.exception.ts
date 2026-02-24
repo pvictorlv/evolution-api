3c804ee30063f1894fed962fb3113bd2364e1e50
Paulo Victor Lund <pvictorlv@gmail.com>
2026-02-24 13:29:31 -0300

    Update 400.exception.ts

--- src/exceptions/400.exception.ts
+++ src/exceptions/400.exception.ts
@@ -2,6 +2,7 @@ import { HttpStatus } from '@api/routes/index.router';
 
 export class BadRequestException {
   constructor(...objectError: any[]) {
+    console.error('BadRequestException:', ...objectError);
     throw {
       status: HttpStatus.BAD_REQUEST,
       error: 'Bad Request',
