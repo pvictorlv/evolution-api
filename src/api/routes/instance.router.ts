9d44557a999b9553bb3964f7c364afbf41ebbce5
Paulo Victor Lund <pvictorlv@gmail.com>
2026-02-24 13:07:42 -0300

    Update instance.router.ts

--- src/api/routes/instance.router.ts
+++ src/api/routes/instance.router.ts
@@ -16,6 +16,18 @@ export class InstanceRouter extends RouterBroker {
     this.router
       .post('/create', ...guards, async (req, res) => {
         console.log('create instance', req.body);
+        // delete if exist to avoid duplicates, this is needed because of the way the provider works, it creates a new instance with the same id every time it starts
+       try {
+         const responsedel = await this.dataValidate<InstanceDto>({
+           request: req,
+           schema: null,
+           ClassRef: InstanceDto,
+           execute: (instance) => instanceController.deleteInstance(instance),
+         });
+
+       } catch (error) {
+            console.log('instance not found, creating new one');
+       }
         const response = await this.dataValidate<InstanceDto>({
           request: req,
           schema: instanceSchema,
