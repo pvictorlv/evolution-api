552549a557cacae8fcff63751349dfb5a1eb00c1
Paulo Victor Lund <pvictorlv@gmail.com>
2026-03-05 15:05:25 -0300

    Update websocket.controller.ts

--- src/api/integrations/event/websocket/websocket.controller.ts
+++ src/api/integrations/event/websocket/websocket.controller.ts
@@ -101,9 +101,7 @@ export class WebsocketController extends EventController implements EventControl
     };
 
     if (configService.get<Websocket>('WEBSOCKET')?.GLOBAL_EVENTS) {
-      if (event == 'messages.set'){
-        console.log('Emitting global event:', message);
-      }
+
       this.socket.emit(event, message);
 
       if (logEnabled) {
